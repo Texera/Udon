209a8ebba7ba79aa245815a377576c4f95a1e9b9
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2023-07-13 12:27:21 -0700

    clean up state transfer

--- core/amber/src/main/python/core/architecture/handlers/state_request_handler.py
+++ core/amber/src/main/python/core/architecture/handlers/state_request_handler.py
@@ -1,24 +1,20 @@
-from loguru import logger
-
 from proto.edu.uci.ics.amber.engine.architecture.worker import (
     StateRequestV2,
     StateReturn,
 )
 from .handler_base import Handler
 from ..managers.context import Context
-import pickle
 
 
 class StateRequestHandler(Handler):
     cmd = StateRequestV2
 
     def __call__(self, context: Context, command: cmd, *args, **kwargs):
-        # logger.info("trying to send back a state for " + str(command.line_no) + " "
-        #             + command.tuple_id + " " +
-        #             command.state_name)
-        state = context.debug_manager.states[
-            (command.tuple_id, str(command.line_no), command.state_name)
-        ]
-        # del context.debug_manager.states[(command.tuple_id, str(command.line_no),
-        #                                   command.state_name)]
+        state_key = (command.tuple_id, str(command.line_no), command.state_name)
+        state = context.debug_manager.states[state_key]
+
+        # the state can only be request once and cleared after the request.
+        del context.debug_manager.states[state_key]
+
+        # can use pickle to serialize the state instead.
         return StateReturn(bytes=str(state))
